2bbb15a5115575f5115b7b73fcf129dd2e7c8bf1
Rybensx <anderson_arf@hotmail.es>
2023-06-10 16:11:35 -0600

    creacion del header, footer y el menu de navegacion

--- DB_final_ramos.sql
+++ DB_final_ramos.sql
@@ -1,19 +1,17 @@
 /* Creación de tabla empleados */
 CREATE TABLE empleados (
-    empleado_id SERIAL NOT NULL,
-    empleado_nom1 VARCHAR(100) NOT NULL,
-    empleado_nom2 VARCHAR(100) NOT NULL,
-    empleado_ape1 VARCHAR(100) NOT NULL,
-    empleado_ape2 VARCHAR(100) NOT NULL,
-    empleado_dpi INTEGER NOT NULL,
-    empleado_edad INTEGER NOT NULL,
-    empleado_puesto_id INTEGER NOT NULL,
-    empleado_sexo_id INTEGER NOT NULL,
-    empleado_area_id INTEGER NOT NULL,
-    PRIMARY KEY(empleado_id),
-    FOREIGN KEY (empleado_area_id) REFERENCES areas(area_id),
-    FOREIGN KEY (empleado_puesto_id) REFERENCES puestos(puesto_id),
-    FOREIGN KEY (empleado_sexo_id) REFERENCES sexo(sexo_id)
+    emp_id SERIAL NOT NULL,
+    emp_nom1 VARCHAR(100) NOT NULL,
+    emp_nom2 VARCHAR(100) NOT NULL,
+    emp_ape1 VARCHAR(100) NOT NULL,
+    emp_ape2 VARCHAR(100) NOT NULL,
+    emp_dpi INTEGER NOT NULL,
+    emp_edad INTEGER NOT NULL,
+    emp_puesto_id INTEGER NOT NULL,
+    emp_sexo_id INTEGER NOT NULL,    
+    PRIMARY KEY(emp_id),    
+    FOREIGN KEY (emp_puesto_id) REFERENCES puestos(puesto_id),
+    FOREIGN KEY (emp_sexo_id) REFERENCES sexo(sexo_id)
 );
 
 /* Creación de tabla sexo */
@@ -38,11 +36,11 @@ CREATE TABLE areas  (
     PRIMARY KEY(area_id)
 );
 
-/* Creación de tabla asignaciones */
-CREATE TABLE asignaciones  ( 
-    asignacion_area_id INTEGER NOT NULL,
-    asignacion_empleado_id INTEGER NOT NULL,
-    PRIMARY KEY(asignacion_area_id, asignacion_empleado_id),
-    FOREIGN KEY (asignacion_area_id) REFERENCES areas(area_id),
-    FOREIGN KEY (asignacion_empleado_id) REFERENCES empleados(empleado_id)
+/* Creación de tabla asignacion_puestos_areas */
+CREATE TABLE asignacion_puestos_areas (
+    area_id INTEGER NOT NULL,
+    puesto_id INTEGER NOT NULL,
+    PRIMARY KEY(area_id, puesto_id),
+    FOREIGN KEY (area_id) REFERENCES areas(area_id),
+    FOREIGN KEY (puesto_id) REFERENCES puestos(puesto_id)
 );
---
+++ inlcudes/navbar.php
@@ -0,0 +1,55 @@
+<nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">INGESOFT S.A.</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Areas
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_ramos/vistas/areas/index.php">Ingresar area</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_ramos/vistas/areas/buscar.php">Buscar area</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Empleados
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_ramos/vistas/empleados/index.php">Ingresar empleado</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_ramos/vistas/empleados/buscar.php">Buscar empleado</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Puestos
+                    </a>
+                    <ul class="dropdown-menu bg-dark">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_ramos/vistas/puestos/index.php">Ingresar puesto</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/final_ramos/vistas/puestos/buscar.php">Buscar puesto</a>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</nav>
