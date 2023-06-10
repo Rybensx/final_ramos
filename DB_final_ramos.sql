6a09a890e363bf4ff6edb6f96b66b1e647d082dc
Rybensx <anderson_arf@hotmail.es>
2023-06-10 17:04:40 -0600

    creasmos el modelo para las areas y modificamos la estructura de nuestra base de datos, creando tablas situaciones, ya que las areas pueden estar en varias situaciones diferentes, al igual que los empleados y los puestos

--- DB_final_ramos.sql
+++ DB_final_ramos.sql
@@ -8,10 +8,12 @@ CREATE TABLE empleados (
     emp_dpi INTEGER NOT NULL,
     emp_edad INTEGER NOT NULL,
     emp_puesto_id INTEGER NOT NULL,
-    emp_sexo_id INTEGER NOT NULL,    
-    PRIMARY KEY(emp_id),    
+    emp_sexo_id INTEGER NOT NULL,
+    emp_situacion_id INTEGER NOT NULL,
+    PRIMARY KEY(emp_id),
     FOREIGN KEY (emp_puesto_id) REFERENCES puestos(puesto_id),
-    FOREIGN KEY (emp_sexo_id) REFERENCES sexo(sexo_id)
+    FOREIGN KEY (emp_sexo_id) REFERENCES sexo(sexo_id),
+    FOREIGN KEY (emp_situacion_id) REFERENCES situaciones_emp(situacion_id)
 );
 
 /* Creación de tabla sexo */
@@ -22,19 +24,24 @@ CREATE TABLE sexo  (
 );
 
 /* Creación de tabla puestos */
-CREATE TABLE puestos ( 
+CREATE TABLE puestos (
     puesto_id SERIAL NOT NULL,
     puesto_descr VARCHAR(200) NOT NULL,
     puesto_suel DECIMAL(8,2),
-    PRIMARY KEY(puesto_id)
+    puesto_situacion_id INTEGER NOT NULL,
+    PRIMARY KEY(puesto_id),
+    FOREIGN KEY (puesto_situacion_id) REFERENCES situaciones_puestos(situacion_id)
 );
 
 /* Creación de tabla areas */
-CREATE TABLE areas  ( 
+CREATE TABLE areas (
     area_id SERIAL NOT NULL,
     area_nom VARCHAR(100) NOT NULL,
-    PRIMARY KEY(area_id)
+    area_situacion_id INTEGER NOT NULL,
+    PRIMARY KEY(area_id),
+    FOREIGN KEY (area_situacion_id) REFERENCES situaciones_areas(situacion_id)
 );
+
 
 /* Creación de tabla asignacion_puestos_areas */
 CREATE TABLE asignacion_puestos_areas (
@@ -43,4 +50,25 @@ CREATE TABLE asignacion_puestos_areas (
     PRIMARY KEY(area_id, puesto_id),
     FOREIGN KEY (area_id) REFERENCES areas(area_id),
     FOREIGN KEY (puesto_id) REFERENCES puestos(puesto_id)
+);
+
+/* Creación de tabla situaciones_emp */
+CREATE TABLE situaciones_emp (
+    situacion_id SERIAL NOT NULL,
+    situacion_descr VARCHAR(100) NOT NULL,
+    PRIMARY KEY(situacion_id)
+);
+
+/* Creación de tabla situaciones_puestos */
+CREATE TABLE situaciones_puestos (
+    situacion_id SERIAL NOT NULL,
+    situacion_descr VARCHAR(100) NOT NULL,
+    PRIMARY KEY(situacion_id)
+);
+
+/* Creación de tabla situaciones_areas */
+CREATE TABLE situaciones_areas (
+    situacion_id SERIAL NOT NULL,
+    situacion_descr VARCHAR(100) NOT NULL,
+    PRIMARY KEY(situacion_id)
 );
---
+++ modelos/Areas.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'Conexion.php';
+
+class Cliente extends Conexion{
+    public $area_id;
+    public $area_nom;
+    public $area_situacion;
+    
+
+
+    public function __construct($args = [] )
+    {
+        $this->area_id = $args['area_id'] ?? null;
+        $this->area_nom = $args['area_nom'] ?? '';
+        $this->area_situacion = $args['area_situacion'] ?? '';
+    }
+
+    public function guardar(){
+        $sql = "INSERT INTO areas(area_nom) values('$this->area_nom')";
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function buscar(){
+        $sql = "SELECT * from areas where area_situacion = 1 ";
+
+        if($this->area_nom != ''){
+            $sql .= " and area_nom like '%$this->area_nom%' ";
+        }
+ 
+        if($this->area_id != null){
+            $sql .= " and area_id = $this->area_id ";
+        }
+
+        $resultado = self::servir($sql);
+        return $resultado;
+    }
+
+    public function modificar(){
+        $sql = "UPDATE areas SET area_nom = '$this->area_nom' where area_id = $this->area_id";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+
+    public function eliminar(){
+        $sql = "UPDATE areas SET area_situacion = 0 where area_id = $this->area_id";
+        
+        $resultado = self::ejecutar($sql);
+        return $resultado;
+    }
+}
